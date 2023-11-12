516e4cb42af06e6bf9cdd77395e98a2f52fabf73
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-11-12 12:01:45 +0100

    Add stats canvas for shaders

--- src/components/shaders/FragmentCanvas.tsx
+++ src/components/shaders/FragmentCanvas.tsx
@@ -1,20 +1,26 @@
 'use client'
 
 import { Canvas } from "@react-three/fiber";
-import { ReactNode } from "react";
-
+import { StatsGl } from "@react-three/drei";
+import { ReactNode, useEffect, useRef } from "react";
+import styles from "./FragmentCanvas.module.css";
 
 type FragmentCanvasProps = {
     children: ReactNode
 };
 
 function FragmentCanvas({children}: FragmentCanvasProps) {
+    const canvasRef = useRef<HTMLCanvasElement>(null!);
+    const parentParentRef = useRef<HTMLElement|null>(null);
+    useEffect(() => {
+        parentParentRef.current = canvasRef.current.parentElement;
+      }, [])
     return (
-        <Canvas camera={{ position: [0.0, 0.0, 1.0] }}>
+        <Canvas ref={canvasRef} camera={{ position: [0.0, 0.0, 1.0] }}>
             {children}
+            <StatsGl parent={parentParentRef} className={styles.stats}/>
         </Canvas>
     );
 }
-
 
 export default FragmentCanvas;
