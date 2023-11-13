619f26d05c0e7e9ff019ec8be2cf6bc7951e94a6
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-11-13 18:37:27 +0100

    Revert to default directory for output

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'dist',
     webpack: (config) => {
         config.module.rules.push({
           test: /\.(frag|vert|glsl)$/,
