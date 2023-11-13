5ea445deb849ba8555b71bc6d95ef1b7afae9cfc
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-11-13 18:11:13 +0100

    Add a new component for shaders with managed uniforms

--- src/app/sketches/basic/sketch-2/page.tsx
+++ src/app/sketches/basic/sketch-2/page.tsx
@@ -1,35 +1,10 @@
 'use client'
 
-import { useFrame } from "@react-three/fiber";
-import { ShaderMaterial } from 'three';
-import { useRef } from "react";
 import fragmentShader from './fragment.glsl'
-import Fragment from "@/components/shaders/Fragment";
-import FragmentCanvas from "@/components/shaders/FragmentCanvas";
-
-function MyFragment() {
-  const materialRef = useRef(null!);
-  const uniforms = {
-      u_time: {
-        type: "f",
-        value: 1.0,
-      },
-    };
-
-  useFrame((state) => {
-    const { clock } = state;
-    (materialRef.current as ShaderMaterial).uniforms.u_time.value = clock.getElapsedTime();
-  });
-
-  return (
-    <Fragment fragmentShader={fragmentShader} uniforms={uniforms} ref={materialRef} />
-  )
-}
+import BasicFragmentShader from "@/components/shaders/BasicFragmentShader";
 
 export default function Page() {
   return (
-    <FragmentCanvas>
-      <MyFragment />
-    </FragmentCanvas>
+    <BasicFragmentShader fragmentShader={fragmentShader} />
   )
 }
---
+++ src/components/shaders/ManagedFragmentShader.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useFrame } from "@react-three/fiber";
+import { ShaderMaterial } from 'three';
+import { useRef } from "react";
+import Fragment from "@/components/shaders/Fragment";
+import FragmentCanvas from "@/components/shaders/FragmentCanvas";
+
+type ManagedFragmentShaderProps = {
+    fragmentShader: string,
+}
+
+function FragmentWithManagedUniforms({ fragmentShader }: ManagedFragmentShaderProps) {
+  const materialRef = useRef(null!);
+  const uniforms = {
+      u_time: {
+        type: "f",
+        value: 1.0,
+      },
+    };
+
+  useFrame((state) => {
+    const { clock } = state;
+    (materialRef.current as ShaderMaterial).uniforms.u_time.value = clock.getElapsedTime();
+  });
+
+  return (
+    <Fragment fragmentShader={fragmentShader} uniforms={uniforms} ref={materialRef} />
+  )
+}
+
+export default function ManagedFragmentShader({ fragmentShader }: ManagedFragmentShaderProps) {
+  return (
+    <FragmentCanvas>
+      <FragmentWithManagedUniforms fragmentShader={fragmentShader}  />
+    </FragmentCanvas>
+  )
+}
