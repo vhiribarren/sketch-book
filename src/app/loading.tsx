d8ecc85044613364187f814657ace9ac355c4708
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-11-15 21:32:03 +0100

    Add loading spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,9 @@
+import { Spin } from 'antd';
+import styles from "./loading.module.css";
+
 export default function Loading() {
-    return <h1>Loading...</h1>
-  }
+  return (
+    <Spin size="large" tip="Loading..." wrapperClassName={styles.spinWrapper}>
+      <div className={styles.spinContent} />
+    </Spin>)
+}
