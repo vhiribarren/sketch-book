c878d7f9e63a72f118cdfc81f0060a68d62b217b
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-11-19 10:27:59 +0100

    Simplify white noise shader code

--- src/app/sketches/basic/white-noise/page.tsx
+++ src/app/sketches/basic/white-noise/page.tsx
@@ -1,46 +1,37 @@
 'use client'
 
-import { ForwardedRef, forwardRef, useEffect, useRef, useState } from "react";
+import { ForwardedRef, forwardRef, useCallback, useState } from "react";
 import { ShaderMaterial } from "three";
 import { Group, NumberInput, Slider, Text } from '@mantine/core';
 import Fragment from "@/components/shaders/Fragment";
 import fragmentShader from './fragment.glsl'
 import { FragmentView } from "@/components/shaders/FragmentView";
 
-
-const WhiteNoiseFragment = forwardRef(
-    function WhiteNoiseFragment(props, ref: ForwardedRef<ShaderMaterial>) {
-        const uniforms = {
-            u_frequence: {
-                type: "f",
-                value: 10.0,
-            },
-        };
-        return (
-            <Fragment fragmentShader={fragmentShader} uniforms={uniforms} ref={ref} />
-        )
-    }
-);
-
+const UNIFORMS = {
+    u_frequence: {
+        value: 4.0,
+    },
+};
 
 export default function Page() {
-    const [frequence, setFrequence] = useState<number | string>(4);
-    const materialRef = useRef<ShaderMaterial>(null);
-    useEffect(() => {
-        if ( ! materialRef.current ) {
-            return;
+
+    const [frequence, setFrequence] = useState<number | string>(UNIFORMS.u_frequence.value);
+
+    const materialRef = useCallback((material: ShaderMaterial) => {
+        if (material !== null) {
+            material.uniforms.u_frequence.value = frequence;
         }
-        (materialRef.current as ShaderMaterial).uniforms.u_frequence.value = frequence;
     }, [frequence]);
+
     return (
         <FragmentView
             fragment={
-                <WhiteNoiseFragment ref={materialRef} />
+                <Fragment fragmentShader={fragmentShader} uniforms={UNIFORMS} ref={materialRef} />
             }
             control={<>
                 <Group wrap="nowrap">
-                    <Slider value={typeof frequence === 'string' ? 0 : frequence} style={{ minWidth: 200 }} min={2} max={500} defaultValue={30} onChange={setFrequence} />
-                    <NumberInput value={frequence} onChange={setFrequence} />
+                    <Slider onChange={setFrequence} value={typeof frequence === 'string' ? 0 : frequence} style={{ minWidth: 200 }} min={2} max={500} />
+                    <NumberInput onChange={setFrequence} value={frequence} min={2} max={500} />
                 </Group>
                 <Text>Height frequency</Text>
             </>
