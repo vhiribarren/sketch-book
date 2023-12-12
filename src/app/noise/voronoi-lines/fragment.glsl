71e2c9d2e7e7e9c73888b1932d8d1d1bfe2e93f1
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-12-12 21:22:43 +0100

    Add manhattan distance for Voronoi lines

--- src/app/noise/voronoi-lines/fragment.glsl
+++ src/app/noise/voronoi-lines/fragment.glsl
@@ -2,10 +2,13 @@
 
 uniform uint u_cell_count;
 uniform float u_line_precision;
+uniform int u_distance_type;
 
 varying vec2 v_uv;
 
-const float infinity = 1.0 / 0.0;
+const float INFINITY = 1.0 / 0.0;
+const int DISTANCE_EUCLIDIAN = 0;
+const int DISTANCE_MANHATTAN = 1;
 
 vec2 generate_cell(uint id) {
     return vec2(random(vec2(float(id), 0.0)), random(vec2(float(id), 1.0)));    
@@ -15,16 +18,25 @@ float generate_color(uint id) {
     return random(vec2(id));    
 }
 
+float voronoi_distance(vec2 left, vec2 right) {
+    if (u_distance_type == DISTANCE_MANHATTAN) {
+        vec2 diff = right - left;
+        return abs(diff.x) + abs(diff.y);
+    } else {
+        return distance(left, right);
+    }
+}
+
 void main() {
     vec2 size = gl_FragCoord.xy / v_uv;
     float ratio = size.x / size.y;
     vec2 uv = v_uv * vec2(ratio, 1.0);
 
-    float min_dist = infinity;
-    float snd_min_dist = infinity;
+    float min_dist = INFINITY;
+    float snd_min_dist = INFINITY;
     for (uint i = 0u; i<u_cell_count; i++) {
         vec2 v_cell = generate_cell(i)*ratio;
-        float v_cell_dist = distance(v_cell, uv);
+        float v_cell_dist = voronoi_distance(uv, v_cell);
         if (v_cell_dist < min_dist) {
             snd_min_dist = min_dist;
             min_dist = v_cell_dist;
