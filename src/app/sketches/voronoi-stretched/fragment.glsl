04c2f64d0ea46d6720d72fd5363ac54887118f29
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-12-16 15:05:12 +0100

    Managed to have final view, but still precision issues when grid x is high

--- src/app/sketches/voronoi-stretched/fragment.glsl
+++ src/app/sketches/voronoi-stretched/fragment.glsl
@@ -4,20 +4,24 @@ varying vec2 v_uv;
 
 const float INFINITY = 1.0 / 0.0;
 const float FREQUENCE = 31.0;
-const float LINE_PRECISION = 0.5;
+const float LINE_PRECISION = 0.1;
 
 
 ivec2 uv_to_grid(vec2 uv) {
     //return ivec2(uv/2.0);
     //return ivec2(uv.x, log2(uv.y + 1.0));
-    return ivec2(uv.x, log2(uv.y + 1.0));
+    int grid_y = int(log2(uv.y + 1.0));
+    int grid_x = int(float(uv.x) / exp2(float(grid_y))) ;
+    return ivec2(grid_x, grid_y);
 }
 
 vec2 gen_cell_at_grid(int x, int y) {
     //return 2.0*(vec2(x, y) + 0.5);    
     //return vec2(float(x), exp2(float(y))-1.0)*(vec2(1.0, 1.0));
-    //return vec2(float(x)+0.5, exp2(float(y-1))-1.0) + vec2(0.0, 0.5) * vec2(float(x), exp2(float(y-1)));  
-    return vec2(float(x), exp2(float(y-1))-1.0) + vec2(random(vec2(y, x)), random(vec2(x, y))) * vec2(1.0, exp2(float(y-1)));  
+    //return vec2(float(x)+0.0, exp2(float(y-1))-1.0) + vec2(0.0, 0.0) * vec2(float(x), exp2(float(y-1)));  
+    return vec2(float(x) * exp2(float(y -1)), exp2(float(y-1))-1.0) + vec2(random(vec2(y, x)), random(vec2(x, y))) * vec2(exp2(float(y-1)), exp2(float(y-1)));  
+    //return vec2(float(x) * exp2(float(y -1)), exp2(float(y-1))-1.0);  
+
 }
 
 void main() {
@@ -25,6 +29,7 @@ void main() {
     float canvas_ratio = canvas_size.x / canvas_size.y;
     vec2 uv = v_uv;
     //uv.y = 1.0 - uv.y;
+    //uv -= 0.01;
     uv *= vec2(canvas_ratio, 1.0); // Cancel screen deformation
 
     //TODO faire aussi du voronoi avec des octaves, des grosses cellules replies de petites cellules avec un coeff de ligne qui baisse
@@ -35,9 +40,9 @@ void main() {
     float min_dist = INFINITY;
     float snd_min_dist = INFINITY;
     vec2 min_vcell;
-    int scan_max = int(exp2(float(grid.y))); // 1;
-    for (int i = -scan_max; i <= scan_max; i++) {
-        for (int j = -scan_max; j <= scan_max; j++) {
+    int scan_max = 2; //int(exp2(float(grid.y))); // 1;
+    for (int i = -30; i <= 10; i++) {
+        for (int j = -1; j <= 2; j++) {
             vec2 current_vcell = gen_cell_at_grid(grid.x + i, grid.y + j);
             float vcell_dist = distance(uv, current_vcell);
             if (vcell_dist < min_dist) {
@@ -56,7 +61,7 @@ void main() {
         web_col = 1.0;
     }
     // Worley field
-    float worley_col = 0.4*min_dist;
+    float worley_col = 0.02*min_dist;
     // Grid
     float grid_col = step(0.98, fract(uv.y)) + step(0.98, fract(uv.x));
 
