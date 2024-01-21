6a4cceb7ccf28b90d1ee7ee6b629e70618054e95
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-01-21 18:00:35 +0100

    Reorganize landscape raymarcher control using accordeons

--- src/app/raymarching/value-noise-field/page.tsx
+++ src/app/raymarching/value-noise-field/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { NumberInput, Select, Switch } from "@mantine/core";
+import { Accordion, NumberInput, Select, Switch, Title } from "@mantine/core";
 import fragmentShader from "./fragment.glsl";
 import { FragmentLogic, FragmentView } from "@/components/shaders/FragmentView";
 import styles from "../../../styles/shaderControl.module.css";
@@ -39,41 +39,82 @@ function ValueNoiseRaymarchControl({controlUiTunnel}: FragmentLogic) {
 
     return (
         <ControlUiTunnel>
-            <div className={styles.shaderControlWrapper}>
-                <Select
-                    className={styles.shaderControl}
-                    label="View type"
-                    placeholder="Pick value"
-                    data={VIEW_TYPE}
-                    value={VIEW_TYPE[viewType]}
-                    onChange={(e) => setViewType(VIEW_TYPE.indexOf(e!))}
-                />
-                <NumberInput className={styles.shaderControl} label="Base frequence" onChange={setFreqBase} value={freqBase} min={0.0} step={0.1} decimalScale={2} />
-                <NumberInput className={styles.shaderControl} label="Field Height Coeff" onChange={setHeightCoeff} value={heightCoeff} min={0.0} step={0.1} decimalScale={2} />
-                <NumberInput className={styles.shaderControl} label="Number of frequences" onChange={setFreqCount} value={freqCount} min={1} max={10} allowDecimal={false} />
-                <NumberInput className={styles.shaderControl} label="Lacunarity" onChange={setLacunarity} value={lacunarity} min={0.0} step={0.1} decimalScale={2} />
-                <NumberInput className={styles.shaderControl} label="Gain" onChange={setGain} value={gain} min={0.0} step={0.1} decimalScale={2} />
-                <Switch
-                    className={styles.shaderControl}
-                    label="Increasing steps"
-                    checked={withLinearSteps}
-                    onChange={(e) => setWithLinearSteps(e.currentTarget.checked)}
-                />
-                <NumberInput className={styles.shaderControl} label="Raymarch Max Steps" onChange={setRaymarchMaxSteps} value={raymarchMaxSteps} allowDecimal={false} />
-                <NumberInput className={styles.shaderControl} label="Raymarch Delta" onChange={setRaymarchDelta} value={raymarchDelta} step={0.01} decimalScale={4} />
-                <NumberInput className={styles.shaderControl} label="Focal Length" onChange={setFocalLength} value={focalLength} step={0.1} decimalScale={2} />
-                <NumberInput className={styles.shaderControl} label="Yaw" onChange={setYaw} value={yaw} />
-                <NumberInput className={styles.shaderControl} label="Pitch" onChange={setPitch} value={pitch} />
-                <NumberInput className={styles.shaderControl} label="Shift X" onChange={setShiftX} value={shiftX} step={0.1} decimalScale={2} />
-                <NumberInput className={styles.shaderControl} label="Shift Y" onChange={setShiftY} value={shiftY} step={0.1} decimalScale={2} />
-                <NumberInput className={styles.shaderControl} label="Shift Z" onChange={setShiftZ} value={shiftZ} step={0.1} decimalScale={2} />
-                <Switch
-                    className={styles.shaderControl}
-                    label="With Gizmos"
-                    checked={gizmos}
-                    onChange={(e) => setGizmos(e.currentTarget.checked)}
-                />
-            </div>
+                <Accordion multiple={true}>
+
+                    <Accordion.Item value="Noise" key="Noise">
+                        <Accordion.Control>
+                            <Title order={6}>Noise</Title>
+                        </Accordion.Control>
+                        <Accordion.Panel>
+                            <div className={styles.shaderControlWrapper}>
+                                <NumberInput className={styles.shaderControl} label="Base frequence" onChange={setFreqBase} value={freqBase} min={0.0} step={0.1} decimalScale={2} />
+                                <NumberInput className={styles.shaderControl} label="Field Height Coeff" onChange={setHeightCoeff} value={heightCoeff} min={0.0} step={0.1} decimalScale={2} />
+                                <NumberInput className={styles.shaderControl} label="Number of frequences" onChange={setFreqCount} value={freqCount} min={1} max={20} allowDecimal={false} />
+                                <NumberInput className={styles.shaderControl} label="Lacunarity" onChange={setLacunarity} value={lacunarity} min={0.0} step={0.1} decimalScale={2} />
+                                <NumberInput className={styles.shaderControl} label="Gain" onChange={setGain} value={gain} min={0.0} step={0.1} decimalScale={2} />
+                            </div>
+                        </Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="Raymarching" key="Raymarching">
+                        <Accordion.Control>
+                            <Title order={6}>Raymarching</Title>
+                        </Accordion.Control>
+                        <Accordion.Panel>
+                            <div className={styles.shaderControlWrapper}>
+                                <Select
+                                    className={styles.shaderControl}
+                                    label="View type"
+                                    placeholder="Pick value"
+                                    data={VIEW_TYPE}
+                                    value={VIEW_TYPE[viewType]}
+                                    onChange={(e) => setViewType(VIEW_TYPE.indexOf(e!))}
+                                />
+                                <Switch
+                                    className={styles.shaderControl}
+                                    label="Increasing steps"
+                                    checked={withLinearSteps}
+                                    onChange={(e) => setWithLinearSteps(e.currentTarget.checked)}
+                                />
+                                <NumberInput className={styles.shaderControl} label="Raymarch Max Steps" onChange={setRaymarchMaxSteps} value={raymarchMaxSteps} allowDecimal={false} />
+                                <NumberInput className={styles.shaderControl} label="Raymarch Delta" onChange={setRaymarchDelta} value={raymarchDelta} step={0.01} decimalScale={4} />
+                            </div>
+                        </Accordion.Panel>
+                    </Accordion.Item>
+                    
+                    <Accordion.Item value="Camera" key="Camera">
+                        <Accordion.Control>
+                            <Title order={6}>Camera</Title>
+                        </Accordion.Control>
+                        <Accordion.Panel>
+                            <div className={styles.shaderControlWrapper}>
+                                <NumberInput className={styles.shaderControl} label="Focal Length" onChange={setFocalLength} value={focalLength} step={0.1} decimalScale={2} />
+                                <NumberInput className={styles.shaderControl} label="Yaw" onChange={setYaw} value={yaw} />
+                                <NumberInput className={styles.shaderControl} label="Pitch" onChange={setPitch} value={pitch} />
+                                <NumberInput className={styles.shaderControl} label="Shift X" onChange={setShiftX} value={shiftX} step={0.1} decimalScale={2} />
+                                <NumberInput className={styles.shaderControl} label="Shift Y" onChange={setShiftY} value={shiftY} step={0.1} decimalScale={2} />
+                                <NumberInput className={styles.shaderControl} label="Shift Z" onChange={setShiftZ} value={shiftZ} step={0.1} decimalScale={2} />
+                            </div>
+                        </Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="Misc" key="Misc">
+                        <Accordion.Control>
+                            <Title order={6}>Misc</Title>
+                        </Accordion.Control>
+                        <Accordion.Panel>
+                            <div className={styles.shaderControlWrapper}>
+                                <Switch
+                                    className={styles.shaderControl}
+                                    label="With Gizmos"
+                                    checked={gizmos}
+                                    onChange={(e) => setGizmos(e.currentTarget.checked)}
+                                />
+                           </div>
+                        </Accordion.Panel>
+                    </Accordion.Item>
+
+                </Accordion>
         </ControlUiTunnel>
     );
 }
